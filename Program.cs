3c9ecf0e4242e348fefe9338fe3e9ff3bf3db384
rahmat48 <rahmatdw48@gmail.co>
2020-03-21 20:06:53 +0700

    inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            codingan1 taxi = new codingan1();
+
+            taxi.Drivername = "Joko";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ codingan1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Program
+{
+    public class codingan1
+    {
+        public string Drivername { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Nama Driver : {0}", Drivername);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine("Number Of Passenger: {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", Drivername);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang", Drivername);
+        }
+    }
+}
